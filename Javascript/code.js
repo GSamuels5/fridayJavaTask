bb47d424db1bd32df08ef0c19f32f728f5cd9233
GSamuels5 <ghamzahsamuels23@gmail.com>
2023-11-24 12:37:57 +0200

    extras added

--- Javascript/code.js
+++ Javascript/code.js
@@ -1,14 +1,15 @@
 let convertBTn = document.querySelector('button')
 
 let displayLabel = document.querySelector('label')
-
+/*function to convert*/
 function convertToSeconds() {
    let inputELement = +document.querySelector('input').value 
    let conversion =   inputELement*60
    displayLabel.textContent = inputELement + 'minutes is equal to ' + conversion + 'seconds'
 }
-
+//to allow button to convert
 convertBTn.addEventListener('click',convertToSeconds)
+//to clear input 
 let clear = document.querySelector('#clear')
 clear.addEventListener('click', ()=>{
    let inputELement = document.querySelector('input')
